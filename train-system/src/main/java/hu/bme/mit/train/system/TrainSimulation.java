d67cdb74cdc103ed213749f46a144699e55b9154
Sealdolphin <mihalovits.mark@gmail.com>
2020-03-31 16:17:30 +0200

    Added documentation

--- train-system/src/main/java/hu/bme/mit/train/system/TrainSimulation.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainSimulation.java
@@ -5,37 +5,43 @@ import javax.swing.*;
 public class TrainSimulation extends JFrame {
 
     public TrainSimulation(TrainSystem system) {
+        //Initiating layout and default JFrame parameters
         GroupLayout layout = new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
+        setTitle("Train Simulation");
+        //Setting initial speed limit
         system.getSensor().overrideSpeedLimit(50);
-
+        //Creating visual components
         JLabel lbSpeed = new JLabel("0");
-
+        JLabel lbSpeedText = new JLabel("Speed:");
+        JLabel lbLimit = new JLabel("Speed Limit");
+        JLabel lbJoystick = new JLabel("Joystick");
+        JSpinner spLimit = new JSpinner(new SpinnerNumberModel(50,50,120,1));
+        JSlider sliderJoystick = new JSlider(JSlider.HORIZONTAL, -10, 10, 0);
+        //Creating timer
+        /*
+        The timer is set to 1 second delay.
+        Every second it triggers the followSpeed event of the controller
+        It also updates the speed value in the Speed label
+         */
         Timer timer = new Timer(1000, actionEvent -> {
             system.getController().followSpeed();
             lbSpeed.setText(Integer.toString(system.getController().getReferenceSpeed()));
         });
-        timer.start();
-
-        JSpinner spLimit = new JSpinner(new SpinnerNumberModel(50,50,70,1));
+        //Setting up speed limit spinner
         spLimit.addChangeListener(event ->
                 system.getSensor().overrideSpeedLimit((int) spLimit.getValue()));
-
-        JLabel lbSpeedText = new JLabel("Speed:");
-        JSlider sliderJoystick = new JSlider(JSlider.HORIZONTAL, -10, 10, 0);
+        //Creating joytick interface
         sliderJoystick.setMajorTickSpacing(5);
         sliderJoystick.setMinorTickSpacing(1);
         sliderJoystick.setPaintLabels(true);
-
+        //Adding joystick change event -> use sensor's override function
         sliderJoystick.addChangeListener(event ->
                 system.getUser().overrideJoystickPosition(sliderJoystick.getValue()));
 
-        JLabel lbLimit = new JLabel("Speed Limit");
-        JLabel lbJoystick = new JLabel("Joystick");
 
-
+        //Constructing horizontal layout
         layout.setHorizontalGroup(
                 layout.createParallelGroup(GroupLayout.Alignment.CENTER)
                 .addGroup(
@@ -43,7 +49,7 @@ public class TrainSimulation extends JFrame {
                                 .addComponent(lbSpeedText).addGap(10).addComponent(lbSpeed)
                 ).addComponent(lbLimit).addComponent(spLimit).addComponent(lbJoystick).addComponent(sliderJoystick)
         );
-
+        //Constructing vertical layout
         layout.setVerticalGroup(
                 layout.createSequentialGroup()
                 .addGroup(
@@ -57,7 +63,10 @@ public class TrainSimulation extends JFrame {
                         .addComponent(lbJoystick)
                         .addComponent(sliderJoystick)
         );
+
+        //Finalizing size and start timer
         pack();
+        timer.start();
     }
 
 }
